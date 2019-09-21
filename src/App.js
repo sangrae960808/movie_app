3a2505397f14821393f27d507934250af72ec6d1
sangrae <sangrae65@naver.com>
2019-09-22 02:50:31 +0900

    https://github.com/sangrae960808/movie_app.git

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+
+function Food({fav}){
+  
+  return <h2>i like {fav}</h2>;
+}
+const foodlike = [
+
+]
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <h1>hello!!!!!!</h1>
+    <Food fav="chicken" />
+    <Food fav="cowgalbi" />
+    <Food fav="kimchi" />
+
+  
+  </div>;
 }
 
 export default App;
